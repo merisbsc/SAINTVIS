7ca69f28039a5ed0a5c4d3e8906e325d0ba76ef9
merisbsc <m.besic@students.htl-leonding.ac.at>
2019-06-03 18:29:36 +0200

    CSS so gut wie fertig, missing: crew page, email send

---
+++ js/crew.js
@@ -0,0 +1,10 @@
+let meris = "../images/photos/unitedSteps-7.jpg";
+let tobi = "../images/photos/unitedSteps-3.jpg";
+let samu = "../images/photos/unitedSteps-7.jpg";
+let fitzi = "../images/photos/unitedSteps-2.jpg";
+
+let names = {meris, tobi, samu, fitzi};
+
+function changeLeft() {
+
+}
